832ca8a2f170e2deba78bf4af46a7d93c4212f78
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-12-27 01:44:48 +0100

    feat: scaffold CLI

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,32 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Build the project
+    Build,
+    /// Run the project in development mode
+    Dev,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    // You can check for the existence of subcommands, and if found use their
+    // matches just as you would the top level cmd
+    match &cli.command {
+        Commands::Build {} => {
+            println!("Building...");
+        }
+        Commands::Dev {} => {
+            println!("Running in development mode...");
+        }
+    }
+}
