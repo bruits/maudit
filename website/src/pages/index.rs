2664a4cfa7694b1efede0b5050e4dfca90efc493
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2024-12-29 03:57:06 +0100

    feat: some more website progress

--- website/src/pages/index.rs
+++ website/src/pages/index.rs
@@ -8,14 +8,33 @@ pub struct Index;
 
 impl Page for Index {
     fn render(&self, ctx: &mut RouteContext) -> RenderResult {
+        let features = [("Feature 1", "Description 1")]
+            .iter()
+            .map(|(title, description)| {
+                html! {
+                div.card {
+                    h4 { (title) }
+                    p { (description) }
+                }
+                }
+            })
+            .collect::<Vec<_>>();
+
         layout(
             html! {
                 (header(ctx))
                 div.w-screen {
                     div."lg:container".mx-auto.hero-background {
                         div.px-6.py-10.mx-6.my-14 {
-                            h2.text-5xl."w-[24ch]"."xl:w-[30ch]"."mb-1"."leading-[1.15]" { "Lo, " span.text-brand-red { "the still scrolls of the web"} ", unchanging and steadfast, at last!" }
-                            p.opacity-75.italic { "Or, in simpler words, " span.text-brand-red {"a static site generator"} "." }
+                            a.bg-our-black.text-our-white.rounded-sm.px-2.py-1.text-sm.mb-2.inline-block."hover:text-white"."hover:cursor-pointer" href="#" {
+                                "Maudit v0.1.0 is out →"
+                            }
+                            h2.text-5xl."w-[24ch]"."xl:w-[30ch]"."mb-1"."leading-[1.15]" {
+                                "Lo, " span.text-brand-red { "the still scrolls of the web"} ", unchanging and steadfast, at last!"
+                            }
+                            p.opacity-75.italic {
+                                "Or, in simpler words, " span.text-brand-red {"a static site generator"} "."
+                            }
                             div.mt-6.leading-tight {
                                 a.btn.block.group href="/docs" { "Get Started" span.inline-block."group-hover:translate-x-4".transition-transform.translate-x-2 { "→" } }
                                 span.opacity-75.italic { "or scroll down to learn more" }
@@ -23,7 +42,14 @@ impl Page for Index {
                         }
                     }
                 }
-                div.h-12.bg-our-black.bg-opacity-5 {}
+                div.h-12.bg-gradient-to-b."from-[#e6e6e3]"{}
+
+                div."px-12"."lg:container".mx-auto {
+                    section {
+                        h3.text-3xl."mb-4" { "1. Features" }
+                        p { "TODO: Find a subtitle" }
+                    }
+                }
 
             },
             ctx,
