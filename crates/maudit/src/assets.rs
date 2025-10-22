5e9fc15fc1fa7a8f20f3ad261904235debf6d3db
Princesseuh <3019731+Princesseuh@users.noreply.github.com>
2025-10-22 03:10:34 +0200

    fix: revert Tailwind fix

--- crates/maudit/src/assets.rs
+++ crates/maudit/src/assets.rs
@@ -346,11 +346,6 @@ fn calculate_hash(path: &Path, options: Option<&HashConfig>) -> String {
             }
             HashAssetType::Style(opts) => {
                 buf.push(opts.tailwind as u8);
-
-                if opts.tailwind {
-                    // If Tailwind is enabled for that style, we can't calculate the hash purely from the file content, as the final output will depend on the Tailwind configuration and the content of the entire project. So we'll add a random thing to the hash to ensure always a new hash. The way to fix this would be to calculate the hash based on the final output CSS after Tailwind processing, but that'd require holding a lot of pages in memory, transforming after we're done with bundling all assets, etc. Which: Does not feel good. Until a better solution, the hash changing on each build when Tailwind is involved will have to do.
-                    buf.extend_from_slice(&Instant::now().elapsed().as_nanos().to_le_bytes());
-                }
             }
             HashAssetType::Script => { /* No extra options for scripts yet */ }
         }
